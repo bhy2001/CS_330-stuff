757c8a9b2f08ee7a14da6f67e439a4085a6d1b34
bhy2001 <88808104+bhy2001@users.noreply.github.com>
2022-11-13 19:19:10 -0600

    geo exercise, 1st submission and minor fixes on jokes-api

--- exercises/geo/app.py
+++ exercises/geo/app.py
@@ -1,3 +1,4 @@
+import sqlite3
 import requests
 from flask import Flask, request, render_template, send_from_directory
 from flask import redirect, url_for
@@ -5,29 +6,90 @@ import records
 
 app = Flask(__name__)
 
+CACHE = {}
+
 
 def get_data_from_db(query: str) -> list:
     """retrieve data from the database and return to the user"""
-    raise NotImplementedError
+    # raise NotImplementedError
+    result = set()
+    local_connect = sqlite3.connect("world.sqlite3")
+    local_cur = local_connect.cursor()
+    local_table = query
+    if local_table:
+        local_cur.execute(query)
+    else:
+        return result.append({'Error': "table name not in database"})
+    for i in local_cur:
+        result.add(i)
+    result = list(result)
+    result.sort()
+    local_cur.close()
+    return result
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    global CACHE
     if request.method == "GET":
         # display links to 3 options (country / region / continent)
-        return render_template("index.html")
-    # retrieve data from the database based on the selected option and present it to the user
+        return render_template("base.html")
+    # retrieve data from the database based on the selected option and present
+    # it to the user
+    if request.form.get("country"):
+        country = request.form.get("country")
+        if country in CACHE:
+            result = CACHE[country]
+        else:
+            query = f"select name, continent, region, capital, surfacearea, population, governmentform, headofstate from country where name = '{country}'"
+            result = get_data_from_db(query)
+            CACHE[country] = result
+        return render_template("index.html", rows=result)
+    if request.form.get("region"):
+        region = request.form.get("region")
+        if region in CACHE:
+            result = CACHE[region]
+        else:
+            query = f"select name, continent, region, capital, surfacearea, population, governmentform, headofstate from country where region = '{region}'"
+            result = get_data_from_db(query)
+            CACHE[region] = result
+        return render_template("index.html", rows=result)
+    if request.form.get("continent"):
+        continent = request.form.get("continent")
+        if continent in CACHE:
+            continent = CACHE[continent]
+        else:
+            query = f"select name, continent, region, capital, surfacearea, population, governmentform, headofstate from country where continent = '{continent}'"
+            result = get_data_from_db(query)
+            CACHE[continent] = result
+        return render_template("index.html", rows=result)
     raise NotImplementedError
 
 
 @app.route("/<string:scope>")
 def search(scope: str):
     if scope == "country":
-        # get countries from the database and populate options of the drop-down menu
+        # get countries from the database and populate options
+        # of the drop-down menu
+        query = "select name from country"
+        country_list = get_data_from_db(query)
+        return render_template("country.html", options=country_list)
         raise NotImplementedError
     elif scope == "region":
-        # get regions from the database and populate options of the drop-down menu
+        # get regions from the database and populate options
+        # of the drop-down menu
+        query = "select region from country"
+        country_list = get_data_from_db(query)
+        return render_template("region.html", options=country_list)
         raise NotImplementedError
     elif scope == "continent":
-        # get continents from the database and populate options of the drop-down menu
+        # get continents from the database and populate options
+        # of the drop-down menu
+        query = "select continent from country"
+        country_list = get_data_from_db(query)
+        return render_template("continent.html", options=country_list)
         raise NotImplementedError
+
+
+if __name__ == "__main__":
+    app.run()
